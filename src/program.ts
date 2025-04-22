ae57f40ff5e7d1fbc9158bc2ed50eaef7f904c3f
qiuz <zhu_anly@163.com>
2025-04-22 14:14:29 +0800

    fix: pnpm alias

--- src/program.ts
+++ src/program.ts
@@ -1,21 +1,21 @@
-import { TaroPlatformBase } from '@tarojs/service'
-import { Template } from './template'
-import { components } from './components'
+import { TaroPlatformBase } from '@tarojs/service';
+import { Template } from './template';
+import { components } from './components';
 
-const PACKAGE_NAME = '@tarojs/plugin-platform-kwai'
+const PACKAGE_NAME = '@tarojs/plugin-platform-kwai';
 
 export default class Kwai extends TaroPlatformBase {
-  platform = 'kwai'
-  globalObject = 'ks'
-  runtimePath = `${PACKAGE_NAME}/dist/runtime`
+  platform = 'kwai';
+  globalObject = 'ks';
+  runtimePath = require.resolve(`${PACKAGE_NAME}/dist/runtime`);
   fileType = {
     templ: '.ksml',
     style: '.css',
     config: '.json',
     script: '.js'
-  }
+  };
 
-  template = new Template()
+  template = new Template();
 
   /**
    * 1. setupTransaction - init
@@ -25,36 +25,36 @@ export default class Kwai extends TaroPlatformBase {
    * 5. build
    * 6. buildTransaction - close
    */
-  constructor (ctx, config) {
-    super(ctx, config)
+  constructor(ctx, config) {
+    super(ctx, config);
 
     this.setupTransaction.addWrapper({
-      close () {
-        this.modifyTemplate()
-        this.generateProjectConfig('project.ks.json')
-        this.generateProjectConfig('project.kwai.json')
+      close() {
+        this.modifyTemplate();
+        this.generateProjectConfig('project.ks.json');
+        this.generateProjectConfig('project.kwai.json');
       }
-    })
+    });
   }
 
   /**
    * 增加组件或修改组件属性
    */
-  modifyTemplate () {
-    const template = this.template
-    template.mergeComponents(this.ctx, components as any)
-    this.modifyInput(template.internalComponents.Input)
+  modifyTemplate() {
+    const template = this.template;
+    template.mergeComponents(this.ctx, components as any);
+    this.modifyInput(template.internalComponents.Input);
   }
 
   /**
    * 修改 Input 组件属性
    */
-  modifyInput (input) {
-    delete input['placeholder-class']
-    delete input['cursor-spacing']
-    delete input['confirm-hold']
-    delete input['cursor']
-    delete input['selection-start']
-    delete input['selection-end']
+  modifyInput(input) {
+    delete input['placeholder-class'];
+    delete input['cursor-spacing'];
+    delete input['confirm-hold'];
+    delete input['cursor'];
+    delete input['selection-start'];
+    delete input['selection-end'];
   }
 }
