2c9f3e791a7acf76e9738c28864591c5b731ff30
qiuz <zhu_anly@163.com>
2025-08-05 17:36:16 +0800

    fix: textarea

--- src/components.ts
+++ src/components.ts
@@ -1,9 +1,5 @@
 import { singleQuote } from '@tarojs/shared';
 
-const _true = 'true';
-const _false = 'false';
-const _empty = '';
-
 export const components = {
   // ======== 调整属性 ========
   Button: {
@@ -26,22 +22,5 @@ export const components = {
   },
   RichText: {
     space: ''
-  },
-  Textarea: {
-    name: _empty,
-    value: _empty,
-    maxlength: 140,
-    disabled: _false,
-    placeholder: _empty,
-    'placeholder-style': _empty,
-    focus: _false,
-    fixed: _false,
-    'cursor-color': "'#FE3666'",
-    'auto-height': _false,
-    'confirm-type': "'return'",
-    'adjust-position': _true,
-    'hold-keyboard': _false,
-    'confirm-hold': _false,
-    bindKeyboardHeightChange: _empty
   }
 };
